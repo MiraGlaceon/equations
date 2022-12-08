2b01e88926f08024f66395f58468037ee5260a78
Denis <jaromirabramov@gmail.com>
2022-12-08 22:20:04 +0600

    init

---
+++ src/main/java/mira/space/EquationsApplication.java
@@ -0,0 +1,11 @@
+package mira.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquationsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EquationsApplication.class, args);
+    }
+}
